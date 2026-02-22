71cfb29faeeeb01c024de86453e7c1daff76f09b
Jorge <Jorge@DESKTOP-1B7HDOA>
2026-02-21 21:14:02 -0500

    Add base commision employee

---
+++ OPPCocepts/OPPCocepts.Backend/BaseCommisionEmployee.cs
@@ -0,0 +1,31 @@
+﻿namespace OPPCocepts.Backend;
+
+public class BaseCommisionEmployee : CommisionEmploye
+{
+    private decimal _salary;
+
+    public BaseCommisionEmployee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date hireDate, float commisionPercent, decimal sales, decimal salary) : base(id, firstName, lastName, isActive, bornDate, hireDate, commisionPercent, sales)
+    {
+        Salary = salary;
+    }
+
+    public decimal Salary 
+    { 
+        get => _salary; 
+        set => _salary = ValidateSalary(value);
+    }
+
+    public override decimal GetValueToPay() => base.GetValueToPay() + Salary;
+
+    override public string ToString() => $"{base.ToString()}" +
+        $"\n\tSalary base......: {Salary,20:C2}";
+
+    private decimal ValidateSalary(decimal salary)
+    {
+        if (salary < 500000)
+        {
+            throw new ArgumentOutOfRangeException(nameof(salary), "Salary base must be greater than or equal to $500,000.");
+        }
+        return salary;
+    }
+}
